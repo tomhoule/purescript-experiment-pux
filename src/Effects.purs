e649d36d5038d676883437d2db016ae5f36cee6f
Tom Houlé <tom@tomhoule.com>
2017-07-28 00:09:01 +0200

    Add the AJAX effect to app effects

--- src/Effects.purs
+++ src/Effects.purs
@@ -3,5 +3,6 @@ module Effects where
 import Control.Monad.Eff.Console (CONSOLE)
 import DOM (DOM)
 import DOM.HTML.Types (HISTORY)
+import Network.HTTP.Affjax (AJAX)
 
-type AppEffects = (console :: CONSOLE, history :: HISTORY, dom :: DOM)
+type AppEffects = (console :: CONSOLE, history :: HISTORY, dom :: DOM, ajax :: AJAX)
